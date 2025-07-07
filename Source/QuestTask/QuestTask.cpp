e6d91903842e6077484d4659794b1e445841d165
ebrahim <e.mottaghi.rezaei@gmail.com>
2025-07-07 20:04:59 +0330

    Add | Initial commit

---
+++ Source/QuestTask/QuestTask.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "QuestTask.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuestTask, "QuestTask" );
